d217ad9e5c75d55864c6d739525ae1200ce2b71f
Timgibtschelle <41118219+MrBlackrelaxo@users.noreply.github.com>
2022-03-20 17:02:10 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    I2C_LCD1602.clear()
+    I2C_LCD1602.ShowString("1", 0, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    I2C_LCD1602.clear()
+    I2C_LCD1602.ShowString("2", 0, 0)
+})
+basic.forever(function () {
+    I2C_LCD1602.LcdInit(39)
+    I2C_LCD1602.on()
+    I2C_LCD1602.BacklightOn()
+})
